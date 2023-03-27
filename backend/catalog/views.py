17e3b3bb5635ea368bbdae56730365e9b7a57eba
poddarswakhar <poddarswakhar97@gmail.com>
2023-03-26 23:00:43 -0700

    some more backend api dev

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
+from .models import Product
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from django.http import HttpResponse
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def products_api(request):
+    """
+    Test will add doc later
+
+    {
+    "productName": "My Product 2",
+    "productOwnerName": "John Doe",
+    "Developers": [
+        "Jane Smith",
+        "Bob Johnson"
+    ],
+    "scrumMasterName": "Alice Lee",
+    "startDate": "2023-03-26",
+    "methodology": "Agile"
+}
+    """
+
+    if request.method == 'GET':
+        data = Product.objects.all()
+        serializer = ProductSerializers(data, context={'request': request}, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializers(data=request.data)
+        if serializer.is_valid():
+            n = serializer.data
+            try:
+                temp_prod = Product(productName=n['productName'], productOwnerName=n['productOwnerName'],
+                                    Developers=n['Developers'], scrumMasterName=n['scrumMasterName'],
+                                    startDate=n['startDate'], methodology=n['methodology'])
+                temp_prod.save()
+                return Response(status=status.HTTP_201_CREATED)
+            except:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    else:
+        return HttpResponse("INVALID REQUEST!")
+
+
+@api_view(['PUT', 'DELETE'])
+def data_del_up(request):
+    try:
+        pk = request.query_params.get('pk')
+        data = Product.objects.get(productId=pk)
+    except data.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = ProductSerializers(data, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- backend/manage.py
+++ backend/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management.commands.runserver import Command as runserver
 
 
 def main():
@@ -9,6 +10,7 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
     try:
         from django.core.management import execute_from_command_line
+        runserver.default_port = "3000"
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
