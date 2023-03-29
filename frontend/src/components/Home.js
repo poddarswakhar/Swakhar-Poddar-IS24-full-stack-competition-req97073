66c9bec9ee5a6b5ea70c1d749b950fffcfd34601
poddarswakhar <poddarswakhar97@gmail.com>
2023-03-28 19:20:14 -0700

    search bar

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,12 +4,12 @@ import ProductList from "./ProductList";
 import NewProductModal from "./NewProductModal";
 
 import axios from "axios";
-
 import { API_URL } from "../constants";
 
 class Home extends Component {
   state = {
-    products: []
+    products: [],
+    searchTerm: "",
   };
 
   componentDidMount() {
@@ -22,11 +22,55 @@ class Home extends Component {
 
   resetState = () => {
     this.getProducts();
+    this.setState({ searchTerm: "" });
+  };
+
+  handleSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  searchProducts = () => {
+    axios.get(API_URL + `?search=${this.state.searchTerm}`).then((res) => {
+      this.setState({ products: res.data });
+    });
   };
 
   render() {
     return (
       <Container style={{ marginTop: "20px" }}>
+        <div style={{ paddingLeft: "32%",}}>
+        <Row>
+            <Col>
+                <input
+                type="text"
+                placeholder="Search Scrum Master or Developer"
+                value={this.state.searchTerm}
+                onChange={this.handleSearchChange}
+                style={{
+                    width: "100%",
+                    borderRadius: "20px",
+                    padding: "10px",
+                    boxSizing: "border-box",
+                }}
+                />
+            </Col>
+            <Col>   
+                <button
+                onClick={this.searchProducts}
+                style={{
+                    marginTop: "0px",
+                    borderRadius: "20px",
+                    padding: "10px 20px",
+                    boxSizing: "border-box",
+                    backgroundColor: "#fcba19"
+                }}
+                >
+                <b>Search</b>
+                </button>
+            </Col>
+        </Row>
+        </div>
+        <br></br><br></br>
         <Row>
           <Col>
             <ProductList
